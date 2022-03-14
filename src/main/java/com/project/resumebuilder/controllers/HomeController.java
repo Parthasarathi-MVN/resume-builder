37320dfbcb426461168a9456263839556dcafe94
Parthasarathi-MVN <mvnparthasarathi@gmail.com>
2022-03-14 17:57:06 +0530

    Added Resume Templates.

--- src/main/java/com/project/resumebuilder/controllers/HomeController.java
+++ src/main/java/com/project/resumebuilder/controllers/HomeController.java
@@ -1,9 +1,12 @@
 package com.project.resumebuilder.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
@@ -16,5 +19,12 @@ public class HomeController {
     public String edit()
     {
         return "edit page";
+    }
+
+    @GetMapping("/view/{userName}")
+    public String view(@PathVariable String userName, Model model)
+    {
+        model.addAttribute("userName", userName); // the String i.e., the first parameter will be used as a variable in the frontend and the second attribute is you get as a parameter to the function
+        return "resume-templates/3/index";
     }
 }
