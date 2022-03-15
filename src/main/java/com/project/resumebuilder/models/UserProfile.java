cd870f6ff2504506e2184982cd99fdb0330ef666
Parthasarathi-MVN <mvnparthasarathi@gmail.com>
2022-03-15 14:22:56 +0530

    Added basic attributes of User Profile.

--- src/main/java/com/project/resumebuilder/models/UserProfile.java
+++ src/main/java/com/project/resumebuilder/models/UserProfile.java
@@ -12,6 +12,52 @@ public class UserProfile {
     private String userName;
     private int themeChoice;
     private String summary;
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    private String email;
+    private String phone;
+    private String designation;
 
     public int getId() {
         return id;
